85fed9f43a49af233c18f2e1f301d375d121252f
swchoi06 <swchoi06@wafflestudio.com>
2013-12-06 02:28:42 +0900

    Changed call_logs_controller

--- app/controllers/call_logs_controller.rb
+++ app/controllers/call_logs_controller.rb
@@ -10,19 +10,21 @@ class CallLogsController < ApplicationController
     call_log.device = params[:device]
     call_log.campus = params[:campus]
     call_log.save
-=begin
-    restaurants = Restaurant.find_all_by_phone_number(@phoneNumber)
-    if restaurants.count == 0
-      restaurant = Restaurant.new
-      restaurant.name = params[:name]
-      restaurant.phone_number = @phoneNumber
-    else
-      restaurant = restaurants[0] 
+
+    if params[:name] != nil
+      restaurants = Restaurant.find_all_by_phone_number(@phoneNumber)
+      if restaurants.count == 0
+        restaurant = Restaurant.new
+        restaurant.name = params[:name]
+        restaurant.phone_number = @phoneNumber
+      else
+        restaurant = restaurants[0] 
+      end
+      restaurant.call_logs.push(call_log)
+      restaurant.campus = call_log.campus
+      restaurant.save
     end
-    restaurant.call_logs.push(call_log)
-    restaurant.campus = call_log.campus
-    restaurant.save
-=end
+
     render :nothing => true
   end
 end
