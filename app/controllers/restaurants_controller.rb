f659c6d6daa7400750bf3d167cf372f60547feb6
swchoi06 <swchoi06@wafflestudio.com>
2014-06-25 01:47:55 +0900

    Simple scaffold for restuarant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,27 +1,74 @@
 class RestaurantsController < ApplicationController
-  skip_before_filter  :verify_authenticity_token
-  def new_menu
-    restaurant = Restaurant.all.select {|r| r.campus == "Yongon" and r.phone_number == params[:phoneNumber].delete(' ')}.first
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
-    if restaurant.menus.count == 0
-       menus = params[:menu]
-       menus.each do |section, menus|
-         menus.each do |menu|
-            m = Menu.new
-            m.section = section
-            m.name = menu[0]
-            m.price = menu[1].to_i
-            m.save
-            restaurant.menus.push(m)
-         end
-       end
-    end
-    render :nothing => true
-  end
-
+  # GET /restaurants
+  # GET /restaurants.json
   def index
+    @restaurants = Restaurant.all
   end
 
+  # GET /restaurants/1
+  # GET /restaurants/1.json
   def show
   end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # GET /restaurants/1/edit
+  def edit
+  end
+
+  # POST /restaurants
+  # POST /restaurants.json
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @restaurant }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /restaurants/1
+  # PATCH/PUT /restaurants/1.json
+  def update
+    respond_to do |format|
+      if @restaurant.update(restaurant_params)
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /restaurants/1
+  # DELETE /restaurants/1.json
+  def destroy
+    @restaurant.destroy
+    respond_to do |format|
+      format.html { redirect_to restaurants_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :phone_number, :campus)
+    end
 end
