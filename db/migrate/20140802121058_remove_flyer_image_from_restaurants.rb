cd770fb170a84edb70c45904b0504bca2d61f08d
swchoi06 <swchoi06@wafflestudio.com>
2014-08-02 21:13:07 +0900

    Check db schema, removed flyer_image

---
+++ db/migrate/20140802121058_remove_flyer_image_from_restaurants.rb
@@ -0,0 +1,5 @@
+class RemoveFlyerImageFromRestaurants < ActiveRecord::Migration
+  def change
+    remove_column :restaurants, :flyer_image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624235019) do
+ActiveRecord::Schema.define(version: 20140802121058) do
 
   create_table "call_logs", force: true do |t|
     t.string   "phoneNumber"
