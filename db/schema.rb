c08103f8201454fd3d51479e7f4433071e0f0f01
swchoi06 <swchoi06@wafflestudio.com>
2014-08-02 21:16:19 +0900

    Change column name for coupon_string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140802121058) do
+ActiveRecord::Schema.define(version: 20140802121518) do
 
   create_table "call_logs", force: true do |t|
     t.string   "phoneNumber"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20140802121058) do
     t.boolean  "flyer",         default: false
     t.string   "flyer_path",    default: ""
     t.boolean  "coupon",        default: false
-    t.string   "coupon_path",   default: ""
+    t.string   "coupon_string", default: ""
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "campus"
