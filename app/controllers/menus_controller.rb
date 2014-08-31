22c6c9ab79f637e48f0bbe58cdccef50fea16b44
swchoi06 <swchoi06@wafflestudio.com>
2014-09-01 00:44:00 +0900

    Solved some security issues

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,5 @@
 class MenusController < ApplicationController
+  before_action :authenticate_admin!, :only => [:index, :show, :edit, :new] 
   before_action :set_menu, only: [:show, :edit, :update, :destroy]
 
   # GET /menus
