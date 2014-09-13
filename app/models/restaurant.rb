442b8fb70804b1a25c484f810ae414c458a86a72
swchoi06 <swchoi06@wafflestudio.com>
2014-09-13 16:16:41 +0900

    Solved some bugs

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,7 +5,8 @@ class Restaurant < ActiveRecord::Base
   serialize :phone_numbers
 
   def hasPhoneNumber?(phone_number)
-    return self.phone_numbers.include?(phone_number)
+    return self.phone_number == phone_number
+#    return self.phone_numbers.include?(phone_number)
   end
 
   def flyers_url
