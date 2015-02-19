d5d17fae1f49b569157df732c3ad0e0a2c3ce22c
Sukwon Choi <swchoi06@wafflestudio.com>
2015-02-19 20:31:39 +0900

    Added call_log to Activeadmin

---
+++ app/admin/call_log.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register CallLog do
+  actions :all, :except => [:new, :show]
+  index do
+    column "Campus" do |call_log|
+      restaurant = call_log.restaurant
+      if not restaurant == nil
+        raw Campus.find_by_id(restaurant.campus_id).name_kor
+      else
+        raw ""
+      end
+    end
+    column "Restaurant" do |call_log|
+      restaurant = call_log.restaurant
+      if not restaurant == nil
+        raw restaurant.name
+      else 
+        raw ""
+      end
+    end
+    column :phoneNumber
+    column :device_type
+    column :created_at
+  end
+end
