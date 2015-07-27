b71f1e318ded3fff4817ba59b859711c038e77ca
Sukwon Choi <swchoi06@wafflestudio.com>
2015-07-27 17:58:40 +0900

    Removed useless files

--- app/uploaders/flyer_uploader.rb
+++ app/uploaders/flyer_uploader.rb
@@ -13,7 +13,7 @@ class FlyerUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "public/flyers/#{model.restaurant.phone_number}"
+    "flyers/#{model.restaurant.id}"
   end
 
   def filename
